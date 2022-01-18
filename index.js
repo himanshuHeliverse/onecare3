e149597cb89e1b767fb660424871f3e2d5fe15bb
himanshu5336234 <himanshu5336234@gmail.com>
2022-01-18 21:16:41 +0530

    carausal commit

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('.responsive').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 100000000000,
+        dots: true,
+        arrows: false,
+      });
+   
+  });
